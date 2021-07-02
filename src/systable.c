9ac45617fcd9da005c5cd52553c66aac7cff3dd2
Tomasz Lemiech <szpajder@gmail.com>
2021-07-02 21:15:50 +0200

    Enclose two systables in the main system table object
    st->current is the system table which gets loaded from a file on startup
    and is then put into use.
    
    st->new is the new system table which is built from decoded System
    Table LPDUs. When the new version is decoded successfully and is
    newer than the current one, it will replace the current one and will
    (optionally) get stored in a file for future use.

--- src/systable.c
+++ src/systable.c
@@ -4,7 +4,7 @@
 #include <libconfig.h>              // config_*
 #include <libacars/dict.h>          // la_dict_*
 #include "systable.h"
-#include "util.h"                   // NEW, XFREE
+#include "util.h"                   // NEW, XFREE, struct location, parse_coordinate
 
 enum systable_err_code {
 	ST_ERR_OK = 0,
@@ -75,12 +75,16 @@ static la_dict const systable_error_messages[] = {
 
 #define STATION_ID_MAX 127
 
+struct _systable {
+	config_t cfg;                                           // system table as a libconfig object
+	char *savefile_path;                                    // where to save updated table
+	config_setting_t const *stations[STATION_ID_MAX+1];     // quick access to GS params (indexed by GS ID)
+	enum systable_err_code err;                             // error code of last operation
+	bool available;                                         // is this system table ready to use
+};
+
 struct systable {
-	config_t cfg;
-	char *savefile_path;
-	config_setting_t const *stations[STATION_ID_MAX+1];
-	enum systable_err_code err;
-	bool available;
+	struct _systable *current, *new;
 };
 
 /******************************
@@ -95,9 +99,11 @@ static bool systable_parse(systable *st);
 
 systable *systable_create(char const *savefile) {
 	NEW(systable, st);
-	config_init(&st->cfg);
+	st->current = XCALLOC(1, sizeof(struct _systable));
+	st->new = XCALLOC(1, sizeof(struct _systable));
+	config_init(&st->current->cfg);
 	if(savefile != NULL) {
-		st->savefile_path = strdup(savefile);
+		st->current->savefile_path = strdup(savefile);
 	}
 	return st;
 }
@@ -107,44 +113,44 @@ bool systable_read_from_file(systable *st, char const *file) {
 		return false;
 	}
 	ASSERT(file);
-	if(config_read_file(&st->cfg, file) != CONFIG_TRUE) {
-		st->err = ST_ERR_LIBCONFIG;
+	if(config_read_file(&st->current->cfg, file) != CONFIG_TRUE) {
+		st->current->err = ST_ERR_LIBCONFIG;
 		return false;
 	}
-	st->available = systable_parse(st);
-	return st->available;
+	st->current->available = systable_parse(st);
+	return st->current->available;
 }
 
 char const *systable_error_text(systable const *st) {
 	if(st == NULL) {
 		return NULL;
 	}
-	ASSERT(st->err < ST_ERR_MAX);
-	if(st->err == ST_ERR_LIBCONFIG) {
-		return config_error_text(&st->cfg);
+	ASSERT(st->current->err < ST_ERR_MAX);
+	if(st->current->err == ST_ERR_LIBCONFIG) {
+		return config_error_text(&st->current->cfg);
 	}
-	return la_dict_search(systable_error_messages, st->err);
+	return la_dict_search(systable_error_messages, st->current->err);
 }
 
 int32_t systable_get_version(systable const *st) {
 	int32_t version = -1;       // invalid default value
 	if(st != NULL) {
-		config_lookup_int(&st->cfg, "version", &version);
+		config_lookup_int(&st->current->cfg, "version", &version);
 	}
 	return version;
 }
 
 char const *systable_get_station_name(systable const *st, int32_t id) {
 	char const *name = NULL;
-	if(st != NULL && id >= 0 && id < STATION_ID_MAX && st->stations[id] != NULL) {
-		config_setting_lookup_string(st->stations[id], "name", &name);
+	if(st != NULL && id >= 0 && id < STATION_ID_MAX && st->current->stations[id] != NULL) {
+		config_setting_lookup_string(st->current->stations[id], "name", &name);
 	}
 	return name;
 }
 
 double systable_get_station_frequency(systable const *st, int32_t gs_id, int32_t freq_id) {
-	if(st != NULL && gs_id >= 0 && gs_id < STATION_ID_MAX && st->stations[gs_id] != NULL) {
-		config_setting_t *frequencies = config_setting_get_member(st->stations[gs_id], "frequencies");
+	if(st != NULL && gs_id >= 0 && gs_id < STATION_ID_MAX && st->current->stations[gs_id] != NULL) {
+		config_setting_t *frequencies = config_setting_get_member(st->current->stations[gs_id], "frequencies");
 		config_setting_t *freq = config_setting_get_elem(frequencies, freq_id);
 		if(freq != NULL) {
 			int type = config_setting_type(freq);
@@ -159,23 +165,27 @@ double systable_get_station_frequency(systable const *st, int32_t gs_id, int32_t
 }
 
 bool systable_is_available(systable const *st) {
-	return st != NULL && st->available;
+	return st != NULL && st->current->available;
 }
 
 void systable_destroy(systable *st) {
 	if(st == NULL) {
-		config_destroy(&st->cfg);
-		XFREE(st->savefile_path);
+		config_destroy(&st->current->cfg);
+		XFREE(st->current->savefile_path);
 		XFREE(st);
 	}
 }
+
+void systable_store_pdu(systable const *st, uint8_t seq_num, uint8_t pdu_seq_len, uint8_t *buf, uint32_t len);
+la_proto_node *systable_decode_from_pdu_sequence(systable const *st);
+
 
 /****************************************
  * Private methods
 *****************************************/
 
 #define parse_success() do { return true; } while(0)
-#define parse_error(code) do { st->err = (code); return false; } while(0)
+#define parse_error(code) do { st->current->err = (code); return false; } while(0)
 
 static bool systable_parse_version(systable *st);
 static bool systable_parse_stations(systable *st);
@@ -199,7 +209,7 @@ static bool systable_parse_version(systable *st) {
 	ASSERT(st);
 
 	int32_t version = 0;
-	if(config_lookup_int(&st->cfg, "version", &version) == CONFIG_FALSE) {
+	if(config_lookup_int(&st->current->cfg, "version", &version) == CONFIG_FALSE) {
 		parse_error(ST_ERR_VERSION_MISSING);
 	}
 	if(version < 0 || version > SYSTABLE_VERSION_MAX) {
@@ -211,7 +221,7 @@ static bool systable_parse_version(systable *st) {
 static bool systable_parse_stations(systable *st) {
 	ASSERT(st);
 
-	config_setting_t *stations = config_lookup(&st->cfg, "stations");
+	config_setting_t *stations = config_lookup(&st->current->cfg, "stations");
 	if(stations == NULL || !config_setting_is_list(stations)) {
 		parse_error(ST_ERR_STATIONS_MISSING);
 	}
@@ -296,10 +306,10 @@ static bool systable_add_station(systable *st, config_setting_t const *station) 
 	int32_t id = 0;
 	// error checking has been done in systable_parse_station()
 	config_setting_lookup_int(station, "id", &id);
-	if(st->stations[id] != NULL) {
+	if(st->current->stations[id] != NULL) {
 		parse_error(ST_ERR_STATION_ID_DUPLICATE);
 	}
-	st->stations[id] = station;
+	st->current->stations[id] = station;
 	parse_success();
 }
 
