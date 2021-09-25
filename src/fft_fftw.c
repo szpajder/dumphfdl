0685cdae3f97976cda1a9de4d908ca82143de07a
Tomasz Lemiech <szpajder@gmail.com>
2021-09-25 19:18:06 +0200

    Make fftw3f_threads library optional
    - pkg_check_modules now checks for fftw3f rather than fftw3f_threads
      (pkgconfig file fftw3f_threads.pc is not installed on some distros)
    - Use find_library() to find libfftw3f_threads. If not found, fall
      back to single-threaded version.

--- src/fft_fftw.c
+++ src/fft_fftw.c
@@ -3,16 +3,21 @@
 #include <fftw3.h>
 #include "fft.h"
 #include "util.h"           // NEW
+#include "config.h"         // WITH_FFTW3F_THREADS
 
 #define FFT_THREAD_CNT 4
 
 void csdr_fft_init() {
+#ifdef WITH_FFTW3F_THREADS
 	fftwf_init_threads();
 	fftwf_plan_with_nthreads(FFT_THREAD_CNT);
+#endif
 }
 
 void csdr_fft_destroy() {
+#ifdef WITH_FFTW3F_THREADS
 	fftwf_cleanup_threads();
+#endif
 }
 
 FFT_PLAN_T* csdr_make_fft_c2c(int32_t size, float complex* input, float complex* output, int32_t forward, int32_t benchmark) {
